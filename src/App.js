9a0b4ea1fb928c914668b9ec792de30efe6969c9
jaegwans <skhu201811140@gmail.com>
2022-11-29 15:46:26 +0900

    fix: react-route-dom basename 수정

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import ProductDetail from './pages/ProductDetail';
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
+            <Routes basename="comento-shop">
                 <Route path="/" element={<Home />} />
                 <Route path="product/:productId" element={<ProductDetail />} />
+
                 <Route path="/basket" element={<Basket />} />
             </Routes>
         </BrowserRouter>
